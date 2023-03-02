a1f3a2d8677c89b43df5dfc02898058570a15329
Najib Adan <najibadan@outlook.jp>
2023-03-02 15:04:33 +0300

    implemented scraper class

--- main.py
+++ main.py
@@ -1,58 +1,30 @@
-import requests
 import configparser
 import mysql.connector
 import sys
-import time
+from scraper import Scraper
 
-def insert_row(mydb,question_id,tags,view_count,answer_count,score,created,is_answered):
-    mycursor = mydb.cursor()
-    insert_sql = f"""INSERT INTO `questions` (question_id,tags,view_count,answer_count,score,created,is_answered) 
-                    VALUES ({question_id},'{' '.join(tags)}',{view_count},{answer_count},{score},'{created}','{is_answered}')"""
-    mycursor.execute(insert_sql)
-    mydb.commit()
+def main(conf):
+    HOSTNAME = conf['CONFIG']['host']
+    USERNAME = conf['CONFIG']['user']
+    PASSWORD = conf['CONFIG']['password']
+    DATABASE = conf['CONFIG']['database']
+    API_KEY = conf['CONFIG']['key']
 
-conf = configparser.ConfigParser()
-try:
-    conf.read('CONFIG_VALUES')
-except Exception as e:
-    print('Could not read the configuration file:' + str(e))
-    sys.exit()
+    conn = mysql.connector.connect(
+    host=HOSTNAME,
+    user=USERNAME,
+    password=PASSWORD,
+    database=DATABASE
+    )
+    scraper = Scraper(API_KEY,conn)
+    scraper.scrap()
 
-HOSTNAME = conf['CONFIG']['host']
-USERNAME = conf['CONFIG']['user']
-PASSWORD = conf['CONFIG']['password']
-DATABASE = conf['CONFIG']['database']
-API_KEY = conf['CONFIG']['key']
-
-mydb = mysql.connector.connect(
-  host=HOSTNAME,
-  user=USERNAME,
-  password=PASSWORD,
-  database=DATABASE
-)
-
-url = f'https://api.stackexchange.com/2.3/questions?fromdate=1656633600&todate=1675209600&order=asc&sort=activity&site=stackoverflow&pagesize=100&key={API_KEY}&page='
-has_more = True
-page = 1
-while has_more:
-    response = requests.get(url+str(page))
-    response = response.json()
-    print(f"Fetching page number: {page}")
-    for item in response['items']:
-        question_id = item['question_id']
-        tags = '|'.join(item['tags'])
-        view_count = item['view_count']
-        answer_count = item['answer_count']
-        score = item['score']
-        created = item['creation_date']
-        is_answered = item['is_answered']
-        insert_row(mydb,question_id,tags,view_count,answer_count,score,created,is_answered)
-    has_more = response['has_more']
-    if 'backoff' in response:
-        sleep = response['backoff'] +0.5 # Just to add an extra time to sleep to avoid being banned
-    else:
-        sleep = 0.5
-    print(f'Sleeping for {sleep}')
-    time.sleep(sleep)
-
-print("Successfully loaded the data")
+    print("Successfully loaded the data")
+if __name__ == "__main__":
+    conf = configparser.ConfigParser()
+    try:
+        conf.read('CONFIG_VALUES')
+    except Exception as e:
+        print('Could not read the configuration file:' + str(e))
+        sys.exit()
+    main(conf) 
---
+++ scraper.py
@@ -0,0 +1,54 @@
+import requests 
+import time
+class Scraper():
+    def __init__(self,api,conn=None,url=None):
+        self.API_KEY = api
+        self.conn = conn
+        if url is None:
+            self.URL = f'https://api.stackexchange.com/2.3/questions?fromdate=1656633600&todate=1675209600&order=asc&sort=activity&site=stackoverflow&pagesize=100&key={self.API_KEY}&page='
+        else:
+            self.URL = url
+    def insert_row(self,question_id,tags,view_count,answer_count,score,created,is_answered):
+        mycursor = self.conn.cursor()
+        insert_sql = f"""INSERT INTO `questions` (question_id,tags,view_count,answer_count,score,created,is_answered) 
+                        VALUES ({question_id},'{' '.join(tags)}',{view_count},{answer_count},{score},'{created}','{is_answered}')"""
+        mycursor.execute(insert_sql)
+        self.conn.commit()
+    
+    def write_to_file(data):
+        with open('stackoverflow_scrap.csv','a') as txt_file:
+            txt_file.write(",".join(str(v) for v in data ) + '\n' )
+    def get_data(url):
+        response = requests.get(url)
+        response = response.json()
+        print(f"Fetching page number: {url.split('=')[-1]}")
+        return response
+    
+    def scrap(self):
+        if self.conn:
+            to = 'db'
+        else:
+            to = 'file'
+        has_more = True 
+        page = 1
+        while has_more:
+            response = self.get_data(self.URL+str(page))
+            for item in response['items']:
+                question_id = item['question_id']
+                tags = '|'.join(item['tags'])
+                view_count = item['view_count']
+                answer_count = item['answer_count']
+                score = item['score']
+                created = item['creation_date']
+                is_answered = item['is_answered']
+                if to == 'db':
+                    self.insert_row(question_id,tags,view_count,answer_count,score,created,is_answered)
+                elif to == 'file':
+                    self.write_to_file([question_id,tags,view_count,answer_count,score,created,is_answered])
+            has_more = response['has_more']
+            if 'backoff' in response:
+                sleep = response['backoff'] +0.5 # Just to add an extra time to sleep to avoid being banned
+            else:
+                sleep = 0.5
+            print(f'Sleeping for {sleep}')
+            time.sleep(sleep)
